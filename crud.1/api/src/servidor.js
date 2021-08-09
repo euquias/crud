1d1fcf432eb448b8981b218f020d7054e0f60f1d
Euquias Araujo <willmaglhaes25@gmail.com>
2021-08-09 17:35:31 -0300

    criação de uma api ultilizando, express e nodmon para atualizar

--- crud.1/api/src/servidor.js
+++ crud.1/api/src/servidor.js
@@ -1,11 +1,26 @@
 const porta = 3005
 const express = require('express')
 const app = express()
+const body = require('bady-parser')
+const  bancodedados = require('./bancodedados')
+
+app.use(bodyparser.urlencoded({extended: true}))
 
 app.get('/produtos', (req, res, next)=>{
-    res.send({nome: 'notebook', preco: 123.45}) //converter para djson
+    res.send(bancodedados.getprofutos()) //converter para djson
 })
- 
+
+app.get('/produtos/:id', (req, res, next) =>{
+    res.send(bancodedados.getproduto(req.params.id))
+}
+app.post('/produtos', (req, res, next) => {
+    const produto = bancodedados.salvarprodutos({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //jsom
+})
+
 app.listen(porta, () => {
     console.log(`servido  esta em executando na porta ${porta}.`)
 })
